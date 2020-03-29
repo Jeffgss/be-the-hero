1fdd8b406e9fb7665343c77e59a52e069a71a32b
Jeff <jefferson.santos@maxmilhas.com.br>
2020-03-29 19:38:33 -0300

    Front-end da aplicação

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -2,14 +2,14 @@ const crypto = require('crypto');
 const connection = require('../database/connection');
 
 module.exports = {
-  async index(req, res) {
+  async index(request, response) {
     const ongs = await connection('ongs').select('*');
 
     return res.json(ongs);
   },
 
-  async create(req, res) {
-    const { name, email, whatsapp, city, uf } = req.body;
+  async create(request, response) {
+    const { name, email, whatsapp, city, uf } = request.body;
 
     const id = crypto.randomBytes(4).toString('HEX');
 
